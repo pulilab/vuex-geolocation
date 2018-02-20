d8b6ccca7a65848b2f6089999775299bd728fa67
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-02-20 13:07:58 +0100

    Rewrote the plugin to new approach. added install function and fixed getcurrentPosition

--- src/vuex-geolocation.js
+++ src/vuex-geolocation.js
@@ -1,83 +1,118 @@
-export default {
-  options: {
+function sync (store, options) {
+  if (!(store && store.commit && store.registerModule && store.state)) {
+    throw new TypeError('Invalid Vuex instance provided');
+  }
+
+  const defaults = {
     autoWatch: true,
     moduleName: 'geolocation',
     enableHighAccuracy: true,
     maximumAge: 30000,
     timeout: 27000
-  },
-  store: null,
-  watchID: null,
-  success ({coords: {latitude, longitude}}) {
-    this.store.commit(`${this.options.moduleName}/LOCATION_CHANGED`, {lat: latitude, lng: longitude});
-    this.store.commit(`${this.options.moduleName}/LOCATION_ERROR`, {code: null, message: ''});
-  },
-  error (error) {
-    this.store.commit(`${this.options.moduleName}/LOCATION_ERROR`, error);
-  },
-  setWatchId (id) {
-    this.store.commit(`${this.options.moduleName}/SET_WATCH_ID`, id);
-  },
-  getWatchId () {
-    return this.store.state[this.options.moduleName].watchID;
-  },
-  sync (store, options) {
-    this.options = {...this.options, ...options};
-    this.store = store;
-    store.registerModule(this.options.moduleName, {
-      namespaced: true,
-      state: {
-        lat: null,
-        lng: null,
-        watchID: null,
-        error: {
-          code: null,
-          message: ''
-        }
-      },
-      mutations: {
-        'LOCATION_CHANGED' (state, {lat, lng}) {
-          state.lat = lat;
-          state.lng = lng;
-        },
-        'LOCATION_ERROR' (state, error) {
-          state.error.code = error.code;
-          state.error.message = error.message;
-        },
-        'SET_WATCH_ID' (state, watchID) {
-          state.watchID = watchID;
-        }
+  };
+  options = {...defaults, ...options};
+  const mutations = {
+    'LOCATION_CHANGED' (state, {lat, lng}) {
+      state.lat = lat;
+      state.lng = lng;
+    },
+    'LOCATION_ERROR' (state, error) {
+      state.error.code = error.code;
+      state.error.message = error.message;
+    },
+    'SET_WATCH_ID' (state, watchID) {
+      state.watchID = watchID;
+    }
+  };
+  store.registerModule(options.moduleName, {
+    namespaced: true,
+    state: {
+      lat: null,
+      lng: null,
+      watchID: null,
+      error: {
+        code: null,
+        message: ''
       }
-    });
-    if (this.options.autoWatch) {
-      this.watchPosition();
-    }
-    return this.unsync;
-  },
-  watchPosition () {
-    if (this.getWatchId() !== null && this.getWatchId() !== undefined) {
-      this.error({code: null, message: 'Watch position is already running, unable to start it again'});
-    }
-    if ('geolocation' in navigator) {
-      const id = navigator.geolocation.watchPosition(this.success.bind(this), this.error.bind(this), this.options);
-      this.setWatchId(id);
-    } else {
-      this.error({code: null, message: 'Geolocation API is not present in this browser'});
-    }
-  },
-  clearWatch () {
-    if (this.getWatchId() !== null && this.getWatchId() !== undefined) {
-      navigator.geolocation.clearWatch(this.getWatchId());
-      this.setWatchId(null);
-    }
-  },
-  getCurrentPosition () {
-    navigator.geolocation.watchPosition(this.success.bind(this), this.error.bind(this), this.options);
-  },
-  unsync () {
-    if (this.store) {
-      this.store.unregisterModule(this.options.moduleName);
-    }
-    this.clearWatch();
+    },
+    mutations
+  });
+
+  function success ({coords: {latitude, longitude}}) {
+    store.commit(`${options.moduleName}/LOCATION_CHANGED`, {lat: latitude, lng: longitude});
+    store.commit(`${options.moduleName}/LOCATION_ERROR`, {code: null, message: ''});
   }
+  function error (error) {
+    store.commit(`${options.moduleName}/LOCATION_ERROR`, error);
+  }
+  function setWatchId (id) {
+    store.commit(`${options.moduleName}/SET_WATCH_ID`, id);
+  }
+  function getWatchId () {
+    const moduleName = options.moduleName;
+    if (store && store.state && store.state[moduleName]) {
+      return store.state[moduleName].watchID;
+    }
+  }
+
+  function watchPosition () {
+    if (getWatchId() !== null && getWatchId() !== undefined) {
+      error({code: null, message: 'Watch position is already running, unable to start it again'});
+    }
+    if (navigator && 'geolocation' in navigator) {
+      const id = navigator.geolocation.watchPosition(success, error, options);
+      setWatchId(id);
+    } else {
+      error({code: null, message: 'Geolocation API is not present in this browser'});
+    }
+  }
+  function clearWatch () {
+    const id = getWatchId();
+    if (id !== null && id !== undefined) {
+      navigator.geolocation.clearWatch(id);
+      setWatchId(null);
+    }
+  }
+  function getCurrentPosition (maximumAge) {
+    options.maximumAge = maximumAge;
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+  function unsync () {
+    if (store) {
+      store.unregisterModule(options.moduleName);
+    }
+    clearWatch();
+  };
+
+  if (options.autoWatch) {
+    watchPosition();
+  }
+
+  function install (Vue) {
+    Vue.prototype.$vuexGeolocation = {
+      unsync,
+      watchPosition,
+      clearWatch,
+      getCurrentPosition
+    };
+  }
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'test') {
+    return {
+      install,
+      watchPosition,
+      unsync,
+      getCurrentPosition,
+      clearWatch,
+      getWatchId,
+      setWatchId,
+      error,
+      success,
+      options,
+      mutations
+    };
+  }
+  return install;
 };
+
+export default { sync };
